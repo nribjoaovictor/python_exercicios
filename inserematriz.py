8e19f984f9ce64888ee543b2d214104b31607d79
nribjoaovictor <167269190+nribjoaovictor@users.noreply.github.com>
2024-10-29 12:33:04 -0300

    inserematriz.py

--- inserematriz.py
+++ inserematriz.py
@@ -1,6 +1,5 @@
+
 import random 
-#fazer: usar o print_matriz dentro do main, caso contrário irá retornar none
-#gerar um arquivo e importar as funçoes
 #ver videos sobre arquivos python
 def print_matriz(matriz_print):
     linhas=len(matriz_print)
@@ -15,12 +14,10 @@ def insere(matriz,nova_matriz,entrada,saida):
     linhas_insere=len(nova_matriz)
     colunas_insere=len(nova_matriz[0])
     l=0
-    for x in range(entrada,entrada+linhas_insere-1):
+    for x in range(entrada,entrada+linhas_insere):
         c=0
-        for y in range(saida,saida+colunas_insere-1):
+        for y in range(saida,saida+colunas_insere):
             matriz[x][y]=nova_matriz[l][c]
-            entrada+=1
-            saida+=1
             c+=1
         l+=1
     return matriz     
@@ -32,22 +29,3 @@ def geramatriz(linha,coluna,minimo,maximo):
             col_matriz.append(random.randint(minimo,maximo))
         li_matriz.append(col_matriz)
     return li_matriz
-def main():
-    qnt_linhas=int(input())
-    qnt_colunas=int(input())
-    min=int(input())
-    max=int(input())
-    matriz=geramatriz(qnt_linhas,qnt_colunas,min,max)
-    print(matriz)
-    qnt_linhas=int(input())
-    qnt_colunas=int(input())
-    min=int(input())
-    max=int(input())
-    nova_matriz=geramatriz(qnt_linhas,qnt_colunas,min,max)
-    print(nova_matriz)
-    entrada=int(input())
-    saida=int(input())
-    matriz_insere=insere(matriz,nova_matriz,2,2)
-    print(matriz_insere)
-    
-main()
